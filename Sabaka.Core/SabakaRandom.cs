b298ef11f7523a208136c7ef9956b26e07efc2e7
sabaka-chabaka <gamekimg360@gmail.com>
2026-02-20 16:41:33 +0400

    Add Sabaka.Core.csproj and SabakaRandom.cs

---
+++ Sabaka.Core/SabakaRandom.cs
@@ -0,0 +1,12 @@
+﻿using SabakaLang;
+
+namespace Sabaka.Core;
+
+[SabakaExport("random")]
+public class SabakaRandom
+{
+    public SabakaRandom() { }
+
+    [SabakaExport("next")]
+    public int Next(int min, int max) => new Random().Next(min, max);
+}
